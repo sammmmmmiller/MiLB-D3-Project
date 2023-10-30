737dc4c7723f0b2a97ab9d090117902abca4728e
Conejo666 <sammiller0013@gmail.com>
2023-10-30 19:47:37 -0400

    2

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const margin = { top: 40, right: 40, bottom: 60, left: 100 };
 const w = 1500 - margin.left - margin.right;
 const h = 1500 - margin.top - margin.bottom;
 
-let svg = d3.select("body").append("svg")
+let container1 = d3.select("body").append("div").classed("svg-container", true)
+let svg = container1.append("svg")
     .attr("width", w + margin.left + margin.right)
     .attr("height", h + margin.top + margin.bottom)
     .style("background-color", "antiquewhite")
@@ -50,7 +51,17 @@ d3.csv('data/average_both.csv').then(function (data) {
         .datum(data)
         .attr("fill", "orange")
         .attr("d", area2(data));
+     // Create the "avgmilbpay" area below the line
+     var area4 = d3.area()
+     .x(function (d) { return x(parseInt(d["year"])); })
+     .y0(h) // The bottom of the area
+     .y1(function (d) { return y(d["avgmlbpay"]); });
 
+ // Create the "avgmilbpay" area chart
+ svg.append("path")
+     .datum(data)
+     .attr("fill", "pink")
+     .attr("d", area4(data));
     // Create the "avgmilbpay" area below the line
     var area3 = d3.area()
         .x(function (d) { return x(parseInt(d["year"])); })
@@ -62,6 +73,8 @@ d3.csv('data/average_both.csv').then(function (data) {
         .datum(data)
         .attr("fill", "red")
         .attr("d", area3(data));
+    
+
 
     // Create the "avgrev" line
     svg.append("path")
@@ -96,35 +109,35 @@ d3.csv('data/average_both.csv').then(function (data) {
             .y(function (d) { return y(d["avgmilbpay"]); })
         );
 
-    // Create circles for "avgrev" data points
-    svg.selectAll(".circle")
-        .data(data)
-        .enter().append("circle")
-        .attr("class", "circle")
-        .attr("cx", function (d) { return x(parseInt(d["year"])); })
-        .attr("cy", function (d) { return y(d["avgrev"] * 1000000); })
-        .attr("r", 5) // Adjust the radius of the circles
-        .attr("fill", "steelblue");
+    // // Create circles for "avgrev" data points
+    // svg.selectAll(".circle")
+    //     .data(data)
+    //     .enter().append("circle")
+    //     .attr("class", "circle")
+    //     .attr("cx", function (d) { return x(parseInt(d["year"])); })
+    //     .attr("cy", function (d) { return y(d["avgrev"] * 1000000); })
+    //     .attr("r", 5) // Adjust the radius of the circles
+    //     .attr("fill", "steelblue");
 
-    // Create circles for "avgpay" data points
-    svg.selectAll(".circle-avgpay")
-        .data(data)
-        .enter().append("circle")
-        .attr("class", "circle-avgpay")
-        .attr("cx", function (d) { return x(parseInt(d["year"])); })
-        .attr("cy", function (d) { return y(d["avgpay"] * 1000000); })
-        .attr("r", 5) // Adjust the radius of the circles
-        .attr("fill", "orange");
+    // // Create circles for "avgpay" data points
+    // svg.selectAll(".circle-avgpay")
+    //     .data(data)
+    //     .enter().append("circle")
+    //     .attr("class", "circle-avgpay")
+    //     .attr("cx", function (d) { return x(parseInt(d["year"])); })
+    //     .attr("cy", function (d) { return y(d["avgpay"] * 1000000); })
+    //     .attr("r", 5) // Adjust the radius of the circles
+    //     .attr("fill", "orange");
 
-    // Create circles for "avgmilbpay" data points
-    svg.selectAll(".circle-avgmilbpay")
-        .data(data)
-        .enter().append("circle")
-        .attr("class", "circle-avgmilbpay")
-        .attr("cx", function (d) { return x(parseInt(d["year"])); })
-        .attr("cy", function (d) { return y(d["avgmilbpay"]); })
-        .attr("r", 5) // Adjust the radius of the circles
-        .attr("fill", "red");
+    // // Create circles for "avgmilbpay" data points
+    // svg.selectAll(".circle-avgmilbpay")
+    //     .data(data)
+    //     .enter().append("circle")
+    //     .attr("class", "circle-avgmilbpay")
+    //     .attr("cx", function (d) { return x(parseInt(d["year"])); })
+    //     .attr("cy", function (d) { return y(d["avgmilbpay"]); })
+    //     .attr("r", 5) // Adjust the radius of the circles
+    //     .attr("fill", "red");
 
     // Add X-axis label
     svg.append("text")
@@ -148,8 +161,9 @@ d3.csv('data/average_both.csv').then(function (data) {
     // Legend items
     const legendItems = [
         { label: "Average Revenue", color: "steelblue" },
-        { label: "Average Pay", color: "orange" },
-        { label: "Average Milb Pay", color: "red" },
+        { label: "Average Payroll", color: "orange" },
+        { label: "Average MiLB Pay", color: "red" },
+        {label: "Average MLB Pay", color: "pink"}
     ];
 
     // Add legend items
@@ -177,5 +191,58 @@ svg.selectAll(".x-axis text")
 // Increase the font size of y-axis ticks
 svg.selectAll(".y-axis text")
 .style("font-size", "14px"); // Adjust the font size as needed
+
+const buttonLabels = Array.from({ length: 31 }, (_, i) => `Button ${i + 1}`);
+// const container = d3.select("body").append("div")
+//   .style("text-align", "center"); // Center its content
+
+// container1.append("button")
+//   .classed("circular-button", true)
+//   .append("img")
+//   .attr("src", `assets/images/MLB.png`) // Adjust the image path
+//   .attr("alt", buttonLabels[30]); // Set alt text for accessibility
+  
+const table = container1.append("table")
+  .style("margin", "auto"); // Center the table on the page
+
+const tbody = table.append("tbody");
+
+
+
+for (let row = 0; row <= 15; row++) {
+  const tr = tbody.append("tr");
+  if (row == 0) {
+    tr.append("td")
+    tr.append("td")
+      .style("padding", "5px")
+      .append("button")
+      .classed("circular-button", true)
+      .append("img")
+      .attr("src", `assets/images/MLB.png`) // Adjust the image path
+      .attr("alt", buttonLabels[0]); // Set alt text for accessibility
+    continue;
+  }
+  for (let col = 1; col <= 2; col++) {
+    const index = (row - 1) * 2 + col - 1;
+    tr.append("td")
+      .style("padding", "5px")
+      .append("button")
+      .classed("circular-button", true)
+      .append("img")
+      .attr("src", `assets/images/Team${index + 1}.png`) // Adjust the image path
+      .attr("alt", buttonLabels[index]); // Set alt text for accessibility
+  }
+}
+
+// Create a div element as a rectangle in container1
+container1.append("div")
+  .style("width", "300px")
+  .style("height", "150px")
+  .style("background-color", "antiquewhite")
+  .style("border", "2px solid navy")
+//   .
+  .style("text-align", "center")
+  .style("font-size", "18px")
+  .text("Your text goes here");
 
 });
